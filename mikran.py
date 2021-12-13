dce0dc2b7e1e33574f22cc32de5a53a5e393c148
Tomasz Zdanowski <zdanozdan@macbook-2.home>
2021-12-13 16:54:59 +0100

    progress

--- mikran.py
+++ mikran.py
@@ -8,17 +8,79 @@ client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-@app.message("nz")
-def new_order(message, say):
-    say(f"Nowe zamówienie")
-    db.new_order()
+@app.command("/nowe-zamowienie")
+#def test_command(body, client, ack, logger):
+def new_order_command(body, ack, respond, client, logger):
+    order = db.new_order()
+    ack(
+        text="Poszło!",
+        blocks=[
+            {
+                "type": "section",
+                "block_id": "b",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":white_check_mark: Accepted!",
+                },
+            }
+        ],
+    )
 
-@app.message("lz")
-def orders_list(message, say):
+    respond(
+        blocks=[
+            {
+                "type": "section",
+                "block_id": "b",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":white_check_mark: Nowe zamówienie utworzone. ",
+                },
+                "accessory": {
+                    "type": "button",
+                    "action_id": "nowe-zamowienie",
+                    "text": {"type": "plain_text", "text": "Edytuj zamówienie"},
+                    "action_id": "button_click",
+                    "value":order['order_id']
+                },
+            }
+        ]
+    )
+
+@app.command("/nowe-zamowienie-x")
+def handle_orders_list(ack, body, logger):
+    #ack(f"Tworzę nowe zamówienie")
+    #order = db.new_order()
+    #info = "Zamówienie z %s, nr (%s) dla %s. Utworzył %s" % (order['date'],order['order_id'],order['customer_id'],order['user'])
+    #ack(info)
+
+    block = {
+        "blocks": [
+            {
+		"type": "section",
+		"text": {
+		    "type": "plain_text",
+		    "text": "This is a plain text section block.",
+		    "emoji": True
+		}
+	    },
+	    {
+	        "type": "divider"
+	    },
+        ]
+    }
+        
+
+    #print(block)
+    ack(blocks=[block,])
+
+@app.command("/lista-zamowien")
+def handle_orders_list(ack, body, logger):
+#    print(body)
+#def orders_list(message, say):
     lzs = db.orders_list()
     blocks = []
-    for lz in lzs:
-        info = "Zamówienie z %s, nr (%s) dla %s. Utworzył %s" % (lz['date'],lz['order_id'],lz['customer_id'],lz['user'])
+    for order in lzs:
+        info = "Zamówienie z %s, nr (%s) dla %s. Utworzył %s" % (order['date'],order['order_id'],order['customer_id'],order['user'])
         block = {
             "type": "section",
             #"text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
@@ -27,23 +89,23 @@ def orders_list(message, say):
                 "type": "button",
                 "text": {"type": "plain_text", "text": "Edutuj zamówienie"},
                 "action_id": "button_click",
-                "value":lz['order_id']
+                "value":order['order_id']
             }
         }
         blocks.append(block)
-        if lz['comment']:
+        if order['comment']:
             block = {
                 "type": "section",
-                "text": {"type": "mrkdwn", "text": " > _%s_" % lz['comment']},
+                "text": {"type": "mrkdwn", "text": " > _%s_" % order['comment']},
             }
             
             blocks.append(block)
 
-    say(blocks=blocks)
+    ack(blocks=blocks)
 
 @app.action("button_click")
 def action_button_click(body, ack, say):
-    print(json.dumps(body['actions'], indent=4, sort_keys=True))
+    #print(json.dumps(body['actions'], indent=4, sort_keys=True))
     order_id = body['actions'][0]['value']
     order = db.get_order(order_id)
     ack()
@@ -53,6 +115,7 @@ def action_button_click(body, ack, say):
         # View payload
         view = {
 	    "type": "modal",
+            "callback_id": "view-id",
 	    "title": {
 	        "type": "plain_text",
 		"text": "Zamówienie %s" % order_id,
@@ -60,15 +123,37 @@ def action_button_click(body, ack, say):
 	    },
 	    "submit": {
 	        "type": "plain_text",
-		"text": "Submit",
+		"text": "Zapisz",
 		"emoji": True
 	    },
 	    "close": {
 	        "type": "plain_text",
-	        "text": "Cancel",
+	        "text": "Anuluj",
 	        "emoji": True
 	    },
 	    "blocks": [
+                {
+                    "type":"input",
+                    "block_id":"find-customer",
+                    "element":{
+			"type": "external_select",
+                        "action_id": "action-find-customer",
+			"placeholder": {"type": "plain_text","text": "Znajdź kontrahenta",},
+                        "min_query_length": 3,
+		    },
+                    "label": {"type": "plain_text", "text": "Wyszukaj kontrahenta"},
+                },
+                {
+                    "type": "input",
+                    "block_id": "mes_b",
+                    "element": {
+                        "type": "multi_external_select",
+                        "action_id": "action-find-product",
+                        "placeholder": {"type": "plain_text", "text": "Wybierz produkt"},
+                        "min_query_length": 3,
+                    },
+                    "label": {"type": "plain_text", "text": "Wyszukaj produkt"},
+                },
                 {
 		    "type": "section",
 		    "text": {
@@ -80,11 +165,130 @@ def action_button_click(body, ack, say):
                         "image_url":"https://i.ytimg.com/vi/O-q9qi2F5yk/hqdefault.jpg",
 		        "alt_text": "cute cat"
 		    }
+                },
+                {
+                    "type": "divider",
+	        },
+                {
+		    "type": "section",
+		    "text": {
+			"type": "mrkdwn",
+			"text": "Usuń"
+		    },
+                    "accessory": {
+                        "type": "checkboxes",
+			"options": [
+			    {
+				"text": {
+				    "type": "mrkdwn",
+				    "text": "*Usuń*"
+				},
+				"description": {
+				    "type": "mrkdwn",
+				    "text": "*Zaznacz żeby usunąć to zamówinie i zapisz*"
+				},
+				"value": order_id,
+			    },
+			],
+                        "action_id": "checkbox-delete-action"
+		    }
 		}
 	    ]
         }
     )
-            
+
+@app.options("action-find-product")
+def show_multi_options(ack):
+    ack(
+        {
+            "option_groups": [
+                {
+                    "label": {"type": "plain_text", "text": "Group 1"},
+                    "options": [
+                        {
+                            "text": {"type": "plain_text", "text": "produkt #1"},
+                            "value": "1-1",
+                        },
+                        {
+                            "text": {"type": "plain_text", "text": "produkt #2"},
+                            "value": "1-2",
+                        },
+                    ],
+                },
+                {
+                    "label": {"type": "plain_text", "text": "Group 2"},
+                    "options": [
+                        {
+                            "text": {"type": "plain_text", "text": "#produkt 1. Produkt z bardzo dlugim opisem, nazwa, PLU, stan magazynowy, "},
+                            "value": "2-1",
+                        },
+                    ],
+                },
+            ]
+        }
+    )
+
+@app.view("view-id")
+def view_submission(ack, body, logger):
+    ack({"response_action": "clear"})
+    #print(json.dumps(body['view']['state']['values'], indent=4, sort_keys=True))
+    values = body['view']['state']['values']
+    for key,value in values.items():
+        try:
+            checkbox = value['checkbox-delete-action']
+            if len(checkbox['selected_options']) > 0:
+                order_id = checkbox['selected_options'][0]['value']
+                db.delete_order(order_id)
+                ack({
+                    "response_action": "update",
+                    "view": {
+                        "type": "modal",
+                        "title": {
+                            "type": "plain_text",
+                            "text": "Updated view"
+                        },
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Naprawdę zmieniłem się i nigdy nie będe taki sam !"
+                            }
+                        }
+                    ]
+                    }
+                })
+        except:
+            pass
+
+
+@app.options("action-find-customer")
+def handle_some_options(ack):
+    ack(
+        {
+            "options": [
+                {
+                    "text": {
+                        "type": "plain_text","text": "Unexpected sentience",
+                    },
+                    "value": "v1"
+                },
+                {
+                    "text": {
+                        "type": "plain_text", "text": "Bot biased toward other bots"
+                    },
+                    "value": "v2"
+                },
+                {
+                    "text": {
+                        "type": "plain_text", "text": "Bot broke my toaster, Bot biased toward other bots."
+                    },
+                    "value": "v3"
+                }
+            ]
+        }
+    )
+
 
 # Start your app
 if __name__ == "__main__":
